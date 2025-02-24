f5a3b111e2ba799aa484b1b0fb243f757a744886
SayemAhmed <sayemahmed070059@gmail.com>
2025-02-24 22:41:41 +0600

    Add files via upload

---
+++ JS BMI Calculator/script.js
@@ -0,0 +1,35 @@
+let height = document.getElementById('height');
+let weight = document.getElementById('weight');
+let button = document.getElementById('button');
+
+let score = document.getElementById('score');
+let result = document.querySelector('.result');
+
+button.addEventListener('click', function () {
+  let newHeight = parseFloat(height.value);
+  let newweight = parseFloat(weight.value);
+  newHeight = newHeight / 100;
+  let sqrHeight = newHeight * newHeight;
+
+  let bmi = newweight / sqrHeight;
+  score.textContent= bmi.toFixed(2);
+  result.style.display= 'block';
+
+
+  if(score.textContent < 18.6){
+    score.style.background = '#f9ca24';
+  }
+  else if(score.textContent < 24.9){
+    score.style.background=  '#badc58';
+  }
+  else{
+    score.style.background = '#d63031';
+  }
+
+});
+
+
+let form = document.getElementById('form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+});
